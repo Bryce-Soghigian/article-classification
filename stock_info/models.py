8e79586ceb90d71a77220e6a2ea2be72ce6db9f4
bsoghigian <bsoghigian@gmail.com>
2021-10-10 02:22:37 -0500

    migrations for stock_info configured

--- stock_info/models.py
+++ stock_info/models.py
@@ -1,3 +1,23 @@
 from django.db import models
+from django.db.models.deletion import CASCADE
+from django.db.models.fields.related import ForeignKey
 
 # Create your models here.
+class Stock(models.Model):
+    ticker = models.TextField(primary_key=True)
+    security_name = models.TextField()
+    market_catagory = models.TextField()
+    
+    def __repr__(self) -> str:
+        return f'ASSET__{self.ticker}'
+
+class StockInfo(models.Model):
+    ticker = models.ForeignKey(Stock, on_delete=CASCADE)
+    date = models.DateField()
+    open_price = models.FloatField()
+    high = models.FloatField()
+    low = models.FloatField()
+    close = models.FloatField()
+    volume = models.FloatField()
+    def __repr__(self) -> str:
+        return f'ASSET_INFO__{self.ticker}'
